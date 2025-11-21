88741144637afa3aa05dd27a836338855bad0271
Aryan Bodhe <aryanbodhe7705@gmail.com>
2025-11-21 10:49:38 +0530

    testing bugfix

--- pages/1_Mailing_Preview.py
+++ pages/1_Mailing_Preview.py
@@ -27,8 +27,13 @@ left_col, center_col, right_col = st.columns([1.2,5,2.5])
 dl: DataLoader = st.session_state['dataloader_instance']
 
 if 'mailer_service' not in st.session_state or st.session_state['mailer_service'] is None:
-    ms: MailerService = MailerService(sender_email=creds.email, app_password=encryption_service.decrypt(creds.app_password))
-    st.session_state['mailer_service'] = ms
+    st.warning(str(creds))
+    try:
+        ms: MailerService = MailerService(sender_email=creds.email, app_password=encryption_service.decrypt(creds.app_password))
+        st.session_state['mailer_service'] = ms
+    except Exception:
+        st.error("Credentials Error.")
+        st.stop()
 else: 
     ms: MailerService = st.session_state['mailer_service']
 
